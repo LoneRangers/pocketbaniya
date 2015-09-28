8022b236bc469c5e05bb4b3acc187b086adc742a
Sujeet Sinha <sujeet1412@gmail.com>
2015-09-28 09:48:26 +0530

    Converted add project into a dialog. Optimized the cancel button functionality

--- app/src/main/java/com/pocketbaniya/sujeet/tripcalculator/AddTrip.java
+++ app/src/main/java/com/pocketbaniya/sujeet/tripcalculator/AddTrip.java
@@ -3,8 +3,11 @@ package com.pocketbaniya.sujeet.tripcalculator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by sujeet on 26/9/15.
@@ -23,8 +26,26 @@ public class AddTrip extends Activity {
 
             @Override
             public void onClick(View view) {
-                Intent mainActivity = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(mainActivity);
+//                Intent mainActivity = new Intent(getBaseContext(), MainActivity.class);
+//                startActivity(mainActivity);
+                finish();
+            }
+        });
+
+        final EditText txtNumberOfFriends = (EditText) findViewById(R.id.txtNumberOfFriends);
+
+        txtNumberOfFriends.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+
+                Editable numberOfFriends = txtNumberOfFriends.getText();
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
         });
     }
